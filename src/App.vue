0e5c6ff93dcbcebff374bf4cf86292121df6cb13
Aneckon <aneckogri@gmail.com>
2022-06-21 16:09:24 +0300

    Fix component: Navbar

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,9 @@ export default {
   <header>
     <div class="wrapper">
       <nav :class="openMenu ? 'header__nav-content block' : 'header__nav-content'">
-        <RouterLink class="navbar__link --left" to="/where">where?</RouterLink>
-        <RouterLink class="navbar__link --right" to="/what">what?</RouterLink>
-        <RouterLink class="navbar__link --bottom" to="/who">who?</RouterLink>
+        <RouterLink class="navbar__link --left" @click="openMenu = false" to="/where">where?</RouterLink>
+        <RouterLink class="navbar__link --right" @click="openMenu = false" to="/what">what?</RouterLink>
+        <RouterLink class="navbar__link --bottom" @click="openMenu = false" to="/who">who?</RouterLink>
       </nav>
       <div @click="openMenu = !openMenu" :class="openMenu ? 'menu__burger open__menu' : 'menu__burger'"></div>
     </div>
