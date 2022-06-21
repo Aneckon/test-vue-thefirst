210bd43e1be00177730ba9ce4863cc6cc761539e
Aneckon <aneckogri@gmail.com>
2022-06-21 16:02:32 +0300

    Verstca: Adaptive done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  data(){
+    return {
+      openMenu: false
+    }
+  }
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
+      <nav :class="openMenu ? 'header__nav-content block' : 'header__nav-content'">
         <RouterLink class="navbar__link --left" to="/where">where?</RouterLink>
         <RouterLink class="navbar__link --right" to="/what">what?</RouterLink>
         <RouterLink class="navbar__link --bottom" to="/who">who?</RouterLink>
       </nav>
+      <div @click="openMenu = !openMenu" :class="openMenu ? 'menu__burger open__menu' : 'menu__burger'"></div>
     </div>
   </header>
 
